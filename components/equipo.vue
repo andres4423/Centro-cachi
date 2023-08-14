91246ab407cc251cd27233a458e6b6af6a3ddc4c
Andres <Olaya4423@gmail.com>
2023-08-14 16:49:49 -0500

    aa

--- components/equipo.vue
+++ components/equipo.vue
@@ -2,11 +2,10 @@
     
     <div class="game">
         <h1>aaaaaaaa</h1>
-<<<<<<< HEAD
+
         <h2>a</h2>
-=======
+
         <h2>la hermana de juanito </h2>
->>>>>>> d4e5a93ccf7bb7ee6fd2cdab280b58eee417c6e9
     </div>
     
 </template>
