313aaa88a95b81a7c4be3d331e045c3704635053
kintanuki <juandiquintanareyes@gmail.com>
2023-08-24 12:27:10 -0500

    equipo

--- components/equipo.vue
+++ components/equipo.vue
@@ -127,27 +127,11 @@
             </div>
         </div>
 
-    <div class="footer-container">
-    
+<Footer></Footer>
+</div>
 
-    <img class="logo" src="../assets/imagenes/logo.png">
 
-  
-    
-        <img class ="celular" src="../assets/imagenes/telefono.png">
-        <p class="cel_p">300 233 6900</p>
-     
-        <img class="correo" src="../assets/imagenes/correo.png"> 
-        <p class="correo_p">centro_psicoterapiaValencia@gmail.com</p>
 
-        <img class="direccion" src="../assets/imagenes/direccion.png">
-        <p class="dir_p">Calle 93 #33A-120</p>
-    <div class="redes">
-
-    </div>
-    </div>
-
-   </div>
     
 </template>
 
