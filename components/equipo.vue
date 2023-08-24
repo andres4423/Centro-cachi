cbd196071bea1796ae21a7437bdc716ebc6ddfcf
kintanuki <juandiquintanareyes@gmail.com>
2023-08-24 11:58:23 -0500

    equipo

--- components/equipo.vue
+++ components/equipo.vue
@@ -10,20 +10,149 @@
         <br>
         <br>
 
+        
         <div class="doc">
             <div class="do">
-                <img src="../assets/imagenes/doc1.png" >
+                <img src="../assets/imagenes/doc1.png" class="nose">
                 <div class="cua">
-                    siiii
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc2.png" class="nose2">
+                <div class="cua2">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
                 </div>
             </div>
         </div>
+
+ 
+        <div class="doc2">
+            <div class="do2">
+                <img src="../assets/imagenes/doc3.png" class="nose3">
+                <div class="cua3">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc4.png" class="nose4">
+                <div class="cua4">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+        
+        </div>
+
+        <div class="doc3">
+            <div class="do2">
+                <img src="../assets/imagenes/doc5.png" class="nose5">
+                <div class="cua5">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc6.png" class="nose6">
+                <div class="cua6">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+        </div>
+
+        <div class="doc4">
+            <div class="do2">
+                <img src="../assets/imagenes/doc7.png" class="nose7">
+                <div class="cua7">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc8.png" class="nose8">
+                <div class="cua8">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+        </div>
+
+        <div class="doc5">
+            <div class="do2">
+                <img src="../assets/imagenes/doc9.png" class="nose9">
+                <div class="cua9">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc10.png" class="nose10">
+                <div class="cua10">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+        </div>
+
+        <div class="doc6">
+            <div class="do2">
+                <img src="../assets/imagenes/doc11.png" class="nose11">
+                <div class="cua11">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+            <div class="do2">
+                <img src="../assets/imagenes/doc12.png" class="nose12">
+                <div class="cua12">
+                    <h3 class="text">Paula Martinez R</h3>
+                    <h4 class="text2">Psicóloga Clínica</h4>
+                    <h4 class="text3">Psicóloga UC y Diplomada en Psicodiagnóstico infantil. Experiencia en la atención de niños, adolescentes y adultos, desde un enfoque psicoanalítico relacional.</h4> 
+                </div>
+            </div>
+        </div>
+
+    <div class="footer-container">
+    
+
+    <img class="logo" src="../assets/imagenes/logo.png">
+
+  
+    
+        <img class ="celular" src="../assets/imagenes/telefono.png">
+        <p class="cel_p">300 233 6900</p>
+     
+        <img class="correo" src="../assets/imagenes/correo.png"> 
+        <p class="correo_p">centro_psicoterapiaValencia@gmail.com</p>
+
+        <img class="direccion" src="../assets/imagenes/direccion.png">
+        <p class="dir_p">Calle 93 #33A-120</p>
+    <div class="redes">
+
+    </div>
+    </div>
+
    </div>
     
 </template>
 
 <style scoped>
-@import url('./estilos/servicios.css');
+@import url('./estilos/equipo.css');
 </style>
 
 <script>
